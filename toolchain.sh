44d920d01f890d2f6caa4436e082f4a972478029
7dog123 <futuretrunks745@gmail.com>
2022-02-13 16:09:39 -0600

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -13,94 +13,40 @@ BINUTILS_VER=2.36
 GCC_VER=11.1.0
 TARGET=mipsel-none-elf
 
-mkdir -pv {stamps,tarballs}
+wget -q https://ftp.gnu.org/pub/gnu/binutils/binutils-${BINUTILS_VER}.tar.xz
 
-if [ ! -f stamps/binutils-download ]; then
-  wget -q https://ftp.gnu.org/pub/gnu/binutils/binutils-${BINUTILS_VER}.tar.xz
-  touch stamps/binutils-download
-fi
+mkdir -pv binutils-{build,source}
+tar -xf binutils.tar.xz -C binutils-source --strip 1
 
-if [ ! -f stamps/binutils-extract ]; then
-  mkdir -pv binutils-{build,source}
-  tar -xf binutils.tar.xz -C binutils-source --strip 1
-  touch stamps/binutils-extract
-fi
-
-if [ ! -f stamps/binutils-configure ]; then
-  pushd binutils-build
+pushd binutils-build
   ../binutils-${BINUTILS_VER}/configure --quiet \
   --prefix=${MIPSEL} --target=${TARGET} \
   --disable-docs --disable-nls --with-float=soft
-  popd
+make --quiet -j${numproc}
+pushd binutils-build
+make --quiet -j${numproc} install-strip
+pop
 
-  touch stamps/binutils-configure
-fi
+wget -q https://ftp.gnu.org/pub/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz
 
-if [ ! -f stamps/binutils-build ]; then
-  pushd binutils-build
-  make --quiet -j${numproc}
-  popd
+mkdir -pv gcc-{build,source}
+tar -xf gcc.tar.xz -C gcc-source --strip 1
 
-  touch stamps/binutils-build
-fi
+pushd gcc-source
+./contrib/download_prerequisites
+popd
 
-if [ ! -f stamps/binutils-install ]; then
-  pushd binutils-build
-  make --quiet -j${numproc} install-strip
-  popd
-
-  touch stamps/binutils-install
-fi
-
-if [ ! -f stamps/gcc-download ]; then
-  wget -q https://ftp.gnu.org/pub/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz
-  touch stamps/gcc-download
-fi
-
-if [ ! -f stamps/gcc-extract ]; then
-  mkdir -pv gcc-{build,source}
-  tar -xf gcc.tar.xz -C gcc-source --strip 1
-  touch stamps/gcc-extract
-fi
-
-if [ ! -f stamps/download_prerequisites ]; then
-  pushd gcc-source
-  ./contrib/download_prerequisites
-  popd
-
-  touch stamps/download_prerequisites
-fi
-
-if [ ! -f stamps/gcc-configure ]; then
-  pushd gcc-build
+pushd gcc-build
   ../gcc-${GCC_VER}/configure --quiet --prefix=${MIPSEL} \
   --target=${TARGET} --disable-docs --disable-nls \
   --disable-libada --disable-libssp --disable-libquadmath \
   --disable-libstdc++-v3 --with-float=soft \
   --enable-languages=c,c++ --with-gnu-as --with-gnu-ld
-  popd
-
-  touch stamps/gcc-configure
-fi
-
-if [ ! -f stamps/gcc-build ]; then
-  pushd gcc-build
-  make --quiet -j${numproc}
-  popd
-
-  touch stamps/gcc-build
-fi
-
-if [ ! -f stamps/gcc-install ]; then
-  pushd gcc-build
-  make install-strip
-  popd
-
-  touch stamps/gcc-install
-fi
+make --quiet -j${numproc}
+make install-strip
+popd
 
 rm -rf *xz
 rm -rf *-source
 rm -rf *-build
-rm -rf stamps
 exit 0

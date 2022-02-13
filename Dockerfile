a32ed60f5e6494fd6d529c2e0058929d2f9a93e7
7dog123 <futuretrunks745@gmail.com>
2022-02-13 20:15:11 +0000

    build docker

--- Dockerfile
+++ Dockerfile
@@ -2,27 +2,16 @@ FROM ubuntu:20.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-ARG BINUTILS_VERSION=2.36
-ARG GCC_VERSION=11.1.0
-ARG TARGET=mipsel-none-elf
-
 ENV MIPSEL /usr/local/cross-mipsel-none-elf
 ENV PATH $PATH:${MIPSEL}/bin 
+
+RUN mkdir -p /mipsel-none-elf-toolchain
+
+COPY toolchain.sh /mipsel-none-elf-toolchain
+
+WORKDIR . /mipsel-none-elf-toolchain
 
 RUN apt-get update --quiet && apt-get install --quiet -y build-essential wget xz-utils zlib1g-dev tar autoconf automake && \
     apt-get clean
 
-RUN wget -q -O binutils.tar.xz https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz && \
-    wget -q -O gcc.tar.xz https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz && \
-    tar xf binutils.tar.xz && tar xf gcc.tar.xz && rm -rf *xz && cd gcc-${GCC_VERSION} && ./contrib/download_prerequisites
-
-RUN mkdir binutils_mipsel && cd binutils_mipsel && \
-    ../binutils-${BINUTILS_VERSION}/configure --quiet --prefix=${MIPSEL} --target=${GCC_TARGET} \
-    --disable-docs --disable-nls --with-float=soft && \
-    make --quiet -j2 && make install-strip
-
-RUN mkdir gcc_mipsel && cd gcc_mipsel && \
-    ../gcc-${{ env.GCC_VERSION }}/configure --quiet --prefix=${MIPSEL} --target=${GCC_TARGET} \
-    --disable-docs --disable-nls --disable-libada --disable-libssp --disable-libquadmath --disable-libstdc++-v3 \
-    --with-float=soft --enable-languages=c,c++ --with-gnu-as --with-gnu-ld && \
-    make --quiet -j2 && make install-strip
+RUN ./toolchain.sh
---
+++ toolchain.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+set -eu
+
+getnumproc() {
+which getconf >/dev/null 2>/dev/null && {
+	getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1;
+} || echo 1;
+};
+
+numproc=`getnumproc`
+
+BINUTILS_VER=2.36
+GCC_VER=11.1.0
+TARGET=mipsel-none-elf
+BINUTILS="ftp://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VER}.tar.bz2"
+GCC="ftp://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER=}.tar.gz"
+
+mkdir -p {stamps,tarballs}
+
+if [ ! -f stamps/binutils-download ]; then
+  wget "${BINUTILS}" -O "tarballs/$(basename ${BINUTILS})"
+  touch stamps/binutils-download
+fi
+
+if [ ! -f stamps/binutils-extract ]; then
+  mkdir -p binutils-{build,source}
+  tar -xf tarballs/$(basename ${BINUTILS}) -C binutils-source --strip 1
+  touch stamps/binutils-extract
+fi
+
+if [ ! -f stamps/binutils-configure ]; then
+  pushd binutils-build
+  ../binutils-${BINUTILS_VER}/configure --quiet \
+  --prefix=${MIPSEL} --target=${TARGET} \
+  --disable-docs --disable-nls --with-float=soft
+  popd
+
+  touch stamps/binutils-configure
+fi
+
+if [ ! -f stamps/binutils-build ]; then
+  pushd binutils-build
+  make --quiet -j${numproc}
+  popd
+
+  touch stamps/binutils-build
+fi
+
+if [ ! -f stamps/binutils-install ]; then
+  pushd binutils-build
+  make --quiet -j${numproc} install-strip
+  popd
+
+  touch stamps/binutils-install
+fi
+
+if [ ! -f stamps/gcc-download ]; then
+  wget "${GCC}" -O "tarballs/$(basename ${GCC})"
+  touch stamps/gcc-download
+fi
+
+if [ ! -f stamps/gcc-extract ]; then
+  mkdir -p gcc-{build,source}
+  tar -xf tarballs/$(basename ${GCC}) -C gcc-source --strip 1
+  touch stamps/gcc-extract
+fi
+
+if [ ! -f stamps/gcc-configure ]; then
+  pushd gcc-build
+  ../gcc-${GCC_VER}/configure --quiet --prefix=${MIPSEL} \
+  --target=${TARGET} --disable-docs --disable-nls \
+  --disable-libada --disable-libssp --disable-libquadmath \
+  --disable-libstdc++-v3 --with-float=soft \
+  --enable-languages=c,c++ --with-gnu-as --with-gnu-ld
+  popd
+
+  touch stamps/gcc-configure
+fi
+
+if [ ! -f stamps/gcc-build ]; then
+  pushd gcc-build
+  make --quiet -j${numproc}
+  popd
+
+  touch stamps/gcc-build
+fi
+
+if [ ! -f stamps/gcc-install ]; then
+  pushd gcc-build
+  make install-strip
+  popd
+
+  touch stamps/gcc-install
+fi
+
+rm -rf "${SCRIPT_DIR}"/../tools/tarballs
+rm -rf "${SCRIPT_DIR}"/../tools/*-source
+rm -rf "${SCRIPT_DIR}"/../tools/*-build
+rm -rf "${SCRIPT_DIR}"/../tools/stamps
+exit 0
